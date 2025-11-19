05798bd048d25b433e2e61ee343746bd4ba16cb3
jeeya1283 <155947612+jeeya1283@users.noreply.github.com>
2025-11-19 17:19:49 +0530

    Delete lib/firebase_methods.dart

--- lib/firebase_methods.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class DatabaseServices{
-  final CollectionReference propertyCollection = FirebaseFirestore.instance.collection('properties');
-}
